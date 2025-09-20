0393f459dc972c6bb83fe2fbec3f1ee1a338a120
srikk <122169588+py-code314@users.noreply.github.com>
2025-09-20 15:15:32 -0400

    feat: Add disabled state & styles to `Previous` btn

--- src/components/NavButtons.jsx
+++ src/components/NavButtons.jsx
@@ -1,9 +1,12 @@
+// import { useState } from 'react';
 import '../styles/NavButtons.css'
 
-const NavButtons = ({onClick, showSection}) => {
+const NavButtons = ({ onClick, showSection }) => {
+  const isDisabled = showSection.personalDetails
+  
   return ( 
     <div className="btns btns--nav">
-      <button className="btn btn--nav">Previous</button>
+      <button className="btn btn--nav" onClick={onClick} disabled={isDisabled} aria-disabled={isDisabled ? "true" : "false"}>Previous{showSection.education && ': Personal Details'}</button>
       <button className="btn btn--nav" onClick={onClick}>Next{showSection.personalDetails && ': Education'}
       </button>
     </div>
